0e9b71474a7787b05f8aab819416d9d27c3de893
mbdev0 <mustafababar7@gmail.com>
2022-07-26 03:11:12 +0100

    get all signatures, filter afterwards, then push to getTransactions -> v slow

--- sol-viewer-app/controller/txInfoController.js
+++ sol-viewer-app/controller/txInfoController.js
@@ -1,6 +1,9 @@
 const { response } = require('express');
-
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+let startTime = new Date().getTime();
+
+
 let getAllSolTransfers = async(body,res) => {
     const rpc = 'https://coin98.genesysgo.net/';
     const lamports = 1000000000;
@@ -11,7 +14,6 @@ let getAllSolTransfers = async(body,res) => {
     };
 
     async function getSignatures() {
-        
         let fetchreq = async (params) => {
             let resp = await fetch(rpc, {
                 method: 'POST',
@@ -30,8 +32,7 @@ let getAllSolTransfers = async(body,res) => {
             let data = await resp.json();
             for(let result of data['result']){
                 //if (result['confirmationStatus'] == 'finalized' && result['err'] == null) {
-                    signatureArr.push(result['signature'])
-                //}
+                signatureArr.push(result)
             }
         }
 
@@ -40,17 +41,23 @@ let getAllSolTransfers = async(body,res) => {
         }
         let prevArr = [];
         while (true){
-            await fetchreq({"limit":1000,"before":signatureArr[signatureArr.length-1]});
+            await fetchreq({"limit":1000,"before":signatureArr[signatureArr.length-1]['signature']});
             console.log("signatureArr: ", signatureArr.length)
             console.log("prevArr: ", prevArr.length)
             if (prevArr.length == signatureArr.length){
-                console.log('They are equal')
                 break
             }
             prevArr = [...signatureArr];
         }
+
+        signatureArr = signatureArr.filter(result => result['confirmationStatus'] === 'finalized' && result['err'] === null)
+
+
     };
     await getSignatures()
+
+    let getSignaturesTime = new Date().getTime();
+    console.log("get all signatures time: ", getSignaturesTime-startTime)
 
     async function getSolTransfers() {
         let promises = [];
@@ -65,7 +72,7 @@ let getAllSolTransfers = async(body,res) => {
                             "id": 1,
                             "method": "getTransaction",
                             "params": [
-                            signature,
+                            signature['signature'],
                             "json"
                             ]
                         }
@@ -74,9 +81,28 @@ let getAllSolTransfers = async(body,res) => {
             // let tx_info = await tx.json();
             
         };
-        const allPromises = await Promise.all(promises);
-        const getTextPromise = allPromises.map((response) => response.text());
-        const allTextPromises = await Promise.allSettled(getTextPromise);
+        let solTransfersTimePromises = new Date().getTime();
+        console.log("Sol transfer time for pushing promises: ",solTransfersTimePromises-getSignaturesTime)
+
+        textPromises = [];
+        const allPromises = await Promise.allSettled(promises);
+
+        let solTransferFetch = new Date().getTime();
+        console.log("Time taken for all fetch for transactions: ", solTransferFetch-solTransfersTimePromises)
+
+        const fulfilled = allPromises.filter(result => result.status === 'fulfilled').map(result => result.value)
+        for(let x of fulfilled){
+            textPromises.push(x.text())
+        }
+        //console.log(fulfilled)
+        //const getTextPromise = allPromises.map((response) => response.text());
+        console.log('before doing text promises')
+        const allTextPromises = await Promise.allSettled(textPromises);
+
+        let textPromiseTime = new Date().getTime();
+        console.log("Time taken to get text for all the promises: ", textPromiseTime - solTransferFetch)
+
+        console.log('before filtering')
 
         for(let p of allTextPromises){
             let tx_info = JSON.parse(p['value'])
@@ -87,23 +113,21 @@ let getAllSolTransfers = async(body,res) => {
                 if (solAmount==0){continue};
                 //pushes an array = [walletFrom, walletTo, Amount in Sol]
                 sol_transfers.push([tx_info['result']['transaction']['message']['accountKeys'][0], tx_info['result']['transaction']['message']['accountKeys'][1], solAmount]);
-                //console.log('sol-transfer');
+                console.log('sol-transfer');
             } else {
                 console.log('not sol-transfer');
             };
         }
 
+        let filterSolTransfers = new Date().getTime();
+        console.log("Time taken to get filter sol transfers: ", filterSolTransfers-textPromiseTime )
+
         return sol_transfers;
     };
-    //let sol_transfers = await getSolTransfers();
+    let sol_transfers = await getSolTransfers();
     //console.log(signatureArr);
-    res.status(200)
-    //.json(sol_transfers);
+    res.status(200).json(sol_transfers);
 };
-
-
-
-
 
 module.exports = {
     getAllSolTransfers

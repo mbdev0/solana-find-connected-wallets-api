1e8a8738f5b472c8552e68ccc7e53d315f926e77
mbdev0 <mustafababar7@gmail.com>
2022-08-04 05:14:18 +0100

    changed sol transactions to show whether money is coming in or going out

--- sol-viewer-app/controller/txInfoController.js
+++ sol-viewer-app/controller/txInfoController.js
@@ -1,30 +1,38 @@
-const { response } = require('express');
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const {
+    response
+} = require('express');
+const fetch = (...args) => import('node-fetch').then(({
+    default: fetch
+}) => fetch(...args));
 
-let startTime = new Date().getTime();
-
-
-let getAllSolTransfers = async(body,res) => {
+let getAllSolTransfers = async (body, res) => {
     const lamports = 1000000000;
     let sol_transfers = [];
     let failed_sol_transfers = []
-    
 
     async function getSignatures() {
         let offset = 0
         let wallet = body.body.wallet
-        while (true){
+        while (true) {
             let resp = await fetch(`https://public-api.solscan.io/account/solTransfers?account=${wallet}&offset=${offset}&limit=50`);
             data = await resp.json()
-            for(transfer of data['data']){
-                if (transfer.status !== 'Success'){
+            for (transfer of data['data']) {
+                if (transfer.status !== 'Success') {
                     failed_sol_transfers.push(transfer)
                 }
-                let time = new Date(transfer['blockTime']*1000)
+                let time = new Date(transfer['blockTime'] * 1000)
+
+                let amount
+                if (transfer['src'] === wallet){
+                    amount = `-${transfer['lamport'] / lamports}`
+                } else {
+                    amount = `+${transfer['lamport'] / lamports} `
+                }
+
                 const st = {
                     source: transfer['src'],
                     destination: transfer['dst'],
-                    amount: transfer['lamport']/lamports,
+                    amount: amount,
                     date: `${time.toLocaleString('en-GB', { timeZone: 'UTC' })} UTC`,
                     solScanTxLink: `https://solscan.io/tx/${transfer['txHash']}`
                 }
@@ -32,19 +40,16 @@ let getAllSolTransfers = async(body,res) => {
             }
             offset += 50
 
-            if (data['data'].length !== 50 || offset>1000) {
+            if (data['data'].length !== 50 || offset > 2000) {
                 break
             }
-            }
-            
-        };
-        
+        }
+        console.log(sol_transfers.length)
 
+
+    };
     await getSignatures()
-    console.log(sol_transfers.length)
 
-
- 
     res.status(200).json(sol_transfers);
 };
 
